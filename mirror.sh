36b78d36e4b147fea866819c1490cb86a8339670
Stuart Axon <stu.axon@gmail.com>
2017-11-10 21:19:00 +0000

    Start to implement mirroring.

--- mirror.sh
+++ mirror.sh
@@ -6,7 +6,14 @@ export PATH=$PATH:$PWD/bin
 
 
 if [[ $# -eq 0 ]] || [[ $# -eq 1 ]]  ; then
-    echo "$0 <src repo> <dst repo>"
+    echo "$0 <directory of subrepo> <dst repo>"
     echo "Mirror repo and push"
     exit 0
 fi
+
+SUBMOD=$1
+PUSH_REMOTE=$2
+
+cd $SUBMOD
+git fetch -p origin
+git push --mirror
